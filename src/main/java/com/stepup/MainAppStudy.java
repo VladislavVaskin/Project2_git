966edb83bdf5df0a0f1f1830196572224e5b8ebe
Vladislav Vaskin <vvaskin@inno.tech>
2025-10-16 17:52:10 +0300

    New line added

--- src/main/java/com/stepup/MainAppStudy.java
+++ src/main/java/com/stepup/MainAppStudy.java
@@ -3,5 +3,6 @@ package com.stepup;
 public class MainAppStudy {
     public static void main(String[] args) {
         System.out.println("Hello fackin world!");
+        System.out.println("Hello my favorit world!");
     }
 }
