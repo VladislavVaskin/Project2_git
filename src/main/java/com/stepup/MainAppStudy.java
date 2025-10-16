3342f07ada16a9312fe68db7943553d0ef7522d1
Vladislav Vaskin <vvaskin@inno.tech>
2025-10-16 18:19:45 +0300

    New class added. And new line

--- src/main/java/com/stepup/MainAppStudy.java
+++ src/main/java/com/stepup/MainAppStudy.java
@@ -1,10 +1,13 @@
 package com.stepup;
 
+import java.awt.desktop.SystemEventListener;
+
 public class MainAppStudy {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello my favorit world!!!");
         System.out.println("Hello fackin my favorit world! AGAIN!");
+        System.out.println("NEW LINE");
         System.out.println("NEW LINE");
     }
 }
