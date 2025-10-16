0df40186ade8fd991510833b30bf07f8b16fdaf8
Vladislav Vaskin <vvaskin@inno.tech>
2025-10-16 18:17:49 +0300

    New class added

---
+++ src/main/java/com/stepup/Box.java
@@ -0,0 +1,13 @@
+package com.stepup;
+
+public class Box {
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+public void setSize(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/com/stepup/MainAppStudy.java
+++ src/main/java/com/stepup/MainAppStudy.java
@@ -2,7 +2,9 @@ package com.stepup;
 
 public class MainAppStudy {
     public static void main(String[] args) {
-        System.out.println("Hello fackin world!");
-        System.out.println("Hello my favorit world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello my favorit world!!!");
+        System.out.println("Hello fackin my favorit world! AGAIN!");
+        System.out.println("NEW LINE");
     }
 }
